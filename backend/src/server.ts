51c90925adcce96455e5fce59aadf7796b777f21
olateju <sirolateju2022@gmail.com>
2025-05-19 15:15:18 +0100

    added endpoint calls limit middleware. ensure Api Protection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import requestLogger from "./middlewares/requestLogger";
 import routes from "./routes";
-
+import rateLimiter from "./middlewares/rateLimiter";
 
 const app = express();
 const port = 3000;
@@ -9,6 +9,8 @@ const port = 3000;
 app.use(express.json());
 app.use(requestLogger);
 app.use(routes);
+app.use(rateLimiter);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
